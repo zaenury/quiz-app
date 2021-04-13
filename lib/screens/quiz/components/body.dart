a78e532af4288868e543dec400fab40c61fadf49
zaenury <adhiimzaenury@gmail.com>
2021-04-14 07:44:47 +0800

    update quiz screen

--- lib/screens/quiz/components/body.dart
+++ lib/screens/quiz/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/constants.dart';
 import 'package:quiz_app/screens/quiz/components/progress_bar.dart';
+import 'package:quiz_app/screens/quiz/components/question_card.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
 class Body extends StatelessWidget {
@@ -14,11 +15,46 @@ class Body extends StatelessWidget {
       children: [
         WebsafeSvg.asset("assets/icons/bg.svg", fit: BoxFit.fill),
         SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            child: Column(
-              children: [ProgressBar()],
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: ProgressBar(),
+              ),
+              SizedBox(
+                height: kDefaultPadding,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Text.rich(
+                  TextSpan(
+                    text: "Question 1",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        .copyWith(color: kSecondaryColor),
+                    children: [
+                      TextSpan(
+                        text: "/10",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5
+                            .copyWith(color: kSecondaryColor),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(thickness: 1.5),
+              SizedBox(height: kDefaultPadding),
+              Expanded(
+                child: PageView.builder(
+                    itemBuilder: (context, index) => QuestionCard()),
+              )
+            ],
           ),
         )
       ],
